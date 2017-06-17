fc6b70805110de9de0f4ec3ec38ce5352d050334
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-06-17 14:14:43 -0400

    made fetch request

--- js/main.js
+++ js/main.js
@@ -15,3 +15,38 @@
 
 
 // 5. Create a way to listen for a click that will play the song in the audio play
+
+(function () {
+  'use strict';
+
+  var myHeaders = {
+    Authorization: 'token ' + TOKEN
+  };
+
+  // urls
+
+  var searchUrl = 'http://api.soundcloud.com/tracks/13158665?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f';
+
+  // get-elements
+  var searchForm = document.getElementById('search-form');
+
+  // onSubmit
+
+  // searchForm.on('submit' )
+
+
+  // function musicSearch() {
+    fetch(searchUrl).then(function (res) {
+        res.json().then(function (data) {
+            var trackCont = document.createElement('div');
+            var trackName = document.createElement('span');
+            trackName.textContent = data.title;
+            console.log(trackName);
+            searchForm.appendChild(trackCont);
+            trackCont.appendChild(trackName);
+      });
+    });
+
+
+
+}());
