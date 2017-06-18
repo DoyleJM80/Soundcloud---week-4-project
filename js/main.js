754acafff226868ce3db526caf9adcc3e4b9e315
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-06-17 20:15:48 -0400

    got fetch request to work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// stream_url
 /*
   Here is a guide for the steps you could take:
 */
@@ -24,28 +25,53 @@
   };
 
   // urls
-
-  var searchUrl = 'http://api.soundcloud.com/tracks/13158665?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f';
+  var userInput = '';
+  var searchUrl = 'http://api.soundcloud.com/tracks/?client_id=' + TOKEN + '&q=';
+  // var searchUrl = 'http://api.soundcloud.com/tracks/?client_id=' + TOKEN+'&q='+userInput;
 
   // get-elements
   var searchForm = document.getElementById('search-form');
 
   // onSubmit
 
-  // searchForm.on('submit' )
-
-
-  // function musicSearch() {
+  searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    userInput = document.getElementById('submit').value;
+    searchUrl += userInput;
+    // function musicSearch() {
     fetch(searchUrl).then(function (res) {
         res.json().then(function (data) {
-            var trackCont = document.createElement('div');
-            var trackName = document.createElement('span');
-            trackName.textContent = data.title;
-            console.log(trackName);
-            searchForm.appendChild(trackCont);
-            trackCont.appendChild(trackName);
-      });
+          data.forEach(function (item) {
+            // create elements
+            // console.log(item.stream_url);
+              var trackCont = document.createElement('div');
+              var imgBox = document.createElement('div');
+              var artistName = document.createElement('span');
+              var trackName = document.createElement('span');
+              var img = '<img src="https://s-media-cache-ak0.pinimg.com/originals/68/f2/9a/68f29a5da65ace8159029d7473594b14.jpg">';
+              // set attributes
+              trackCont.setAttribute('class', 'track-box');
+              imgBox.setAttribute('class', 'image-box');
+              imgBox.innerHTML = img;
+              trackName.textContent = item.title;
+              //append child
+              searchForm.appendChild(trackCont);
+              trackCont.appendChild(imgBox);
+              trackCont.appendChild(trackName);
+              //fetch stream_url
+              if (item.artwork_url !== null) {
+                img = '<img src="' + item.artwork_url + '">';
+              }
+          });
+        });
     });
+  });
+
+  fetch('https://api.soundcloud.com/tracks/306427043/stream?client_id=' + TOKEN).then(function (result) {
+    // result.json().then(function (data2) {
+      console.log(result);
+    // });
+  });
 
 
 
