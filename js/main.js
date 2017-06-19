8a5ececcf94b42318b57cd8431e9f3f3e48b8f14
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-06-19 08:03:39 -0400

    made title change to track name when played

--- js/main.js
+++ js/main.js
@@ -1,30 +1,19 @@
-// stream_url
-/*
-  Here is a guide for the steps you could take:
-*/
 
-// 1. First select and store the elements you'll be working with
-
-
-// 2. Create your `onSubmit` event for getting the user's search term
-
-
-// 3. Create your `fetch` request that is called after a submission
-
-
-// 4. Create a way to append the fetch results to your page
-
-
-// 5. Create a way to listen for a click that will play the song in the audio play
 
 (function () {
   'use strict';
 
-
-
   var myHeaders = {
     Authorization: 'token ' + TOKEN
   };
+
+  try {
+    myHeaders['Authorization'] = 'token ' + TOKEN;
+  } catch (e) {
+    //ignore error
+  }
+
+
   var img = '<img src="https://s-media-cache-ak0.pinimg.com/originals/68/f2/9a/68f29a5da65ace8159029d7473594b14.jpg">';
   // urls
   var userInput = '';
@@ -32,7 +21,8 @@
   // get-elements
   var searchForm = document.getElementById('search-form');
   var searchResults = document.getElementById('search-results');
-  var newImg = 'bob';
+  var newTitle = document.getElementById('new-title');
+  var newImg = '';
   // onSubmit
   searchForm.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -54,6 +44,7 @@
               imgBox.innerHTML = img;
               imgBox.addEventListener('click', function () {
                 fetchStream(imgBox.dataset.stream);
+                newTitle.textContent = item.title;
               });
               imgBox.dataset.stream = item.stream_url;
               imgBox.dataset.art = item.artwork_url;
@@ -64,11 +55,6 @@
               trackCont.appendChild(trackName);
           });
         });
-        // .then(fetch(item.artwork_url + '?client_id=' + TOKEN).then(function (result2) {
-        //   var imgArt = document.getElementById('image-box');
-        //   newImg = result2.url;
-        //   imgArt.src = newImg;
-        // }));
     });
   });
   function fetchStream(url) {
